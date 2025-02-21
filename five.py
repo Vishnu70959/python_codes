d6bd939e241f459d3027574ecc8ac1b7eb1e524b
Vishnu70959 <144869934+Vishnu70959@users.noreply.github.com>
2025-02-21 15:00:13 +0530

    Add files via upload

---
+++ five.py
@@ -0,0 +1,22 @@
+num=[1,2,3,4,5,3]
+num1=[7,8,9,10 ]
+
+#append
+num.append(6)
+print(num)
+
+#extend
+num.extend(num1)
+print(num)
+
+#pop
+num.pop()
+print(num)
+
+#len
+print(len(num))
+
+#count
+print(num.count(3))
+
+
